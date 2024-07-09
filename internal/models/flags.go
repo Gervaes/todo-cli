068a1dddd565a42d637792708faf031d552d9d34
Gervaes <eogervaes@gmail.com>
2024-07-08 22:02:43 -0300

    TCLI feat: Improve -h text a bit

--- internal/models/flags.go
+++ internal/models/flags.go
@@ -14,12 +14,12 @@ type Flags struct {
 func NewFlags() Flags {
 	flags := Flags{}
 
-	flag.StringVar(&flags.NewDescription, "c", "", "Creates a new todo for today with the description provided")
-	flag.StringVar(&flags.NewNote, "m", "", "Adds a note to a todo. Used together with -u")
-	flag.IntVar(&flags.IdToUpdate, "u", 0, "Updates a todo's status to the next logic one")
-	flag.IntVar(&flags.IdToDelete, "d", 0, "Deletes a todo from the list")
-	flag.BoolVar(&flags.GetAllTodos, "a", false, "Gets all todos not only from today")
-	flag.BoolVar(&flags.ShowTodosNote, "n", false, "Gets todos with notes")
+	flag.StringVar(&flags.NewDescription, "c", "", "Creates a new Todo for today with the description provided.")
+	flag.StringVar(&flags.NewNote, "m", "", "Adds a note to a Todo when used together with -u.")
+	flag.IntVar(&flags.IdToUpdate, "u", 0, "Updates a Todo's status to the next logic one, unless when used with -m, that updates the Todo's note instead.")
+	flag.IntVar(&flags.IdToDelete, "d", 0, "Deletes a Todo from the list.")
+	flag.BoolVar(&flags.GetAllTodos, "a", false, "Gets all Todos not only from today.")
+	flag.BoolVar(&flags.ShowTodosNote, "n", false, "Gets Todos with their note's information.")
 	flag.Parse()
 
 	return flags
