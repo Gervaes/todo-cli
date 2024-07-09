2a6945c7a970f52e34676e0c32d8fde92f4954fe
Gervaes <eogervaes@gmail.com>
2024-07-08 22:38:13 -0300

    TCLI feat: Improve -h text

--- internal/models/flags.go
+++ internal/models/flags.go
@@ -14,12 +14,12 @@ type Flags struct {
 func NewFlags() Flags {
 	flags := Flags{}
 
-	flag.StringVar(&flags.NewDescription, "c", "", "Creates a new Todo for today with the description provided.")
-	flag.StringVar(&flags.NewNote, "m", "", "Adds a note to a Todo when used together with -u.")
-	flag.IntVar(&flags.IdToUpdate, "u", 0, "Updates a Todo's status to the next logic one, unless when used with -m, that updates the Todo's note instead.")
-	flag.IntVar(&flags.IdToDelete, "d", 0, "Deletes a Todo from the list.")
-	flag.BoolVar(&flags.GetAllTodos, "a", false, "Gets all Todos not only from today.")
-	flag.BoolVar(&flags.ShowTodosNote, "n", false, "Gets Todos with their note's information.")
+	flag.StringVar(&flags.NewDescription, "c", "", `Creates a new Todo for today with the description provided. Example: todo -c "review today's PRs"`)
+	flag.StringVar(&flags.NewNote, "m", "", `Adds a note to a Todo when used together with -u. Example: todo -u 8 -m "he's only available after 3PM"`)
+	flag.IntVar(&flags.IdToUpdate, "u", 0, `Updates a Todo's status to the next logic one, unless when used with -m, that updates the Todo's note instead. Example: todo -u 8`)
+	flag.IntVar(&flags.IdToDelete, "d", 0, `Deletes a Todo from the list. Example: todo -d 8`)
+	flag.BoolVar(&flags.GetAllTodos, "a", false, `Gets all Todos not only from today. Example: todo -a`)
+	flag.BoolVar(&flags.ShowTodosNote, "n", false, `Gets Todos with their note's information. Example: todo -n`)
 	flag.Parse()
 
 	return flags
